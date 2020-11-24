a4fc432436721d902e24500ee737318ec9054c83
Patrik Lopes <patriklopes2@outlook.com>
2020-11-23 23:28:12 -0300

    pronto

---
+++ js/size.js
@@ -0,0 +1,15 @@
+var screenWidth = window.innerWidth
+var screenHeight = window.innerHeight
+
+var size = document.querySelector('#size')
+
+size.innerText = `${screenWidth} X ${screenHeight}`
+
+function atualizar(){
+    var screenWidth = window.innerWidth
+    var screenHeight = window.innerHeight
+
+    var size = document.querySelector('#size')
+    size.innerText = `${screenWidth} X ${screenHeight}`
+
+}
